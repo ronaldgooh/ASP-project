0ef98afe4c9a34106e0a1a0ac6a66635f50eb98c
Kitkamseng <64192497+Kitkamseng@users.noreply.github.com>
2022-09-05 03:00:29 +0800

    Dead page

---
+++ src/components/WebView/CommonModules/DeadPage/index.js
@@ -0,0 +1,38 @@
+import './index.css';
+import NavBar from '../Homepage(Consumer)/NavBar(consumer)';
+import Footer from '../Footer';
+import Maintenance from './Maintenance';
+
+const DeadPage = () => {
+    return(
+        <>
+            <NavBar  />
+            <Maintenance />
+            <Footer />
+        </>
+    )
+};
+
+
+
+export default DeadPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
